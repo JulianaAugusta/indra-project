0e852edfd0bf8e71b9451f3d7867a60b0158dadc
JulianaAugusta <juli_aug@hotmail.com>
2020-10-06 07:26:08 -0300

    criação dos componentes de cadastro e acesso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { ClientRegisterComponent } from './cadastro/client-register/client-register.component';
+
+import { AccountAccessComponent } from './account/account-access/account-access.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientRegisterComponent,
+    AccountAccessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'account-access', component: AccountAccessComponent},
+      {path: 'client-register', component: ClientRegisterComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
